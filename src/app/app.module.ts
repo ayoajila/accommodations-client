6e92d9ce3fcfe4d680bffffe694c0d9d2bbc9ff1
Michael Rayson <mrayson5129@gmail.com>
2020-10-08 15:20:11 -0400

    added import for request module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RequestModule } from './request/request.module';
 
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { httpLoaderFactory } from './shared/factories/httpLoaderFactory';
@@ -20,6 +21,7 @@ import { LandingComponent } from './landing/landing.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    RequestModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
