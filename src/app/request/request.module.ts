73c5486baedbbdfb7993d49cde773b0c791f946a
Michael Rayson <mrayson5129@gmail.com>
2020-10-09 12:15:13 -0400

    added routing module for navigation

--- src/app/request/request.module.ts
+++ src/app/request/request.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewRequestComponent } from './new-request/new-request.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app-routing.module';
 
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { httpLoaderFactory } from '../shared/factories/httpLoaderFactory';
 import { HttpClient } from '@angular/common/http';
+import { SelectWorkspaceComponent } from './select-workspace/select-workspace.component';
 
 
 @NgModule({
-  declarations: [NewRequestComponent],
+  declarations: [NewRequestComponent, SelectWorkspaceComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    AppRoutingModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
