744f83bb279894c45503a224292a00418edb4e76
Michael Rayson <mrayson5129@gmail.com>
2020-10-15 16:37:29 -0400

    Added missing imports to test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,24 @@
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+
 import { AppComponent } from './app.component';
+import { httpLoaderFactory } from './shared/factories/httpLoaderFactory';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: httpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })
       ],
       declarations: [
         AppComponent
--- src/app/nav-bar/nav-bar.component.spec.ts
+++ src/app/nav-bar/nav-bar.component.spec.ts
@@ -1,5 +1,8 @@
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 
+import { httpLoaderFactory } from '../shared/factories/httpLoaderFactory';
 import { NavBarComponent } from './nav-bar.component';
 
 describe('NavBarComponent', () => {
@@ -8,7 +11,17 @@ describe('NavBarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavBarComponent ]
+      declarations: [ NavBarComponent ],
+      imports: [
+        HttpClientModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: httpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })
+      ]
     })
     .compileComponents();
   }));
